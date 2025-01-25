0766f4da9b1d5a49b99adedf294b597305647e34
Girts Kesteris <girts.kesteris@gmail.com>
2025-01-25 16:59:28 +0200

    billboarded mexican projectile

--- Assets/_GGJ/Scripts/ProjectileTrajectoryFix.cs
+++ Assets/_GGJ/Scripts/ProjectileTrajectoryFix.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 
 public class ProjectileTrajectoryFix : MonoBehaviour
 {
-    IEnumerator Start()
-    {   
-        yield return new WaitForFixedUpdate();
-        ProjectileMoveScript projectileScript = GetComponent<ProjectileMoveScript>();
+    enum DirType
+    {
+        Target,
+        Camera
+    }
 
-        Vector3 target = projectileScript.target.ToVector();
+    [SerializeField] DirType dirType = DirType.Target;
+
+    private Camera _camera;
+    private Camera Camera
+    {
+        get
+        {
+            if (_camera == null) _camera = Camera.main;
+            return _camera;
+        }
+    }
+    
+    IEnumerator Start()
+    {
+        yield return new WaitForFixedUpdate();
+        Vector3 target = GetTargetPosition();
         transform.forward = (target - transform.position).normalized;
+    }
+
+    Vector3 GetTargetPosition()
+    {
+        switch (dirType)
+        {
+            case DirType.Target:
+                ProjectileMoveScript projectileScript = GetComponent<ProjectileMoveScript>();
+                return projectileScript.target.ToVector();
+
+            case DirType.Camera:
+                return Camera.transform.position;
+                break;
+        }
+        
+        return Vector3.zero;
     }
 }
